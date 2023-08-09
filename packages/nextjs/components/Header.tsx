15f0d3d7c332b1302dcaa45fa02e9d2b4c7698b3
habacuc.eth <habacuc.vera@gmail.com>
2023-08-09 00:27:58 -0600

    UI components draft

--- packages/nextjs/components/Header.tsx
+++ packages/nextjs/components/Header.tsx
@@ -61,7 +61,7 @@ export const Header = () => {
   );
 
   return (
-    <div className="sticky lg:static top-0 navbar bg-base-100 min-h-0 flex-shrink-0 justify-between z-20 shadow-md shadow-secondary px-0 sm:px-2">
+    <div className=" top-0 flex navbar fixed z-20 px-0 sm:px-2">
       <div className="navbar-start w-auto lg:w-1/2">
         <div className="lg:hidden dropdown" ref={burgerMenuRef}>
           <label
