8fa6c789cc523165a3d5a824b1099d0e9aeacdb2
habacuc.eth <habacuc.vera@gmail.com>
2023-08-10 14:20:33 -0600

    Home Button

--- packages/nextjs/pages/index.tsx
+++ packages/nextjs/pages/index.tsx
@@ -5,7 +5,7 @@ import type { NextPage } from "next";
 
 const Home: NextPage = () => {
   return (
-    <div className="m-auto">
+    <div className="m-auto grid items-center">
       <MetaHeader />
       <div className="bg-bt/60 w-screen grid xl:grid-cols-2 text-center py-5">
         <div className="mx-auto grid xl:flex items-center text-l1 text-6xl xl:text-8xl font-kum font-bold gap-5 pb-5 xl:pb-0 px-10">
@@ -16,6 +16,7 @@ const Home: NextPage = () => {
           Crosschain Attestations for web3 onboarding
         </div>
       </div>
+      <div className="homeBT fixed bottom-20 left-1/2 -translate-x-1/2">Connect</div>
     </div>
   );
 };
