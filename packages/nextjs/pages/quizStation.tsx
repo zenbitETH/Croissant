d030e75207646bb98886a8288176c8849acb9657
habacuc.eth <habacuc.vera@gmail.com>
2023-08-12 11:04:51 -0600

    Verify Step 2

--- packages/nextjs/pages/quizStation.tsx
+++ packages/nextjs/pages/quizStation.tsx
@@ -1,11 +1,12 @@
 //import Tutorial from "@/components/Tutorial";
 //import Quiz from "@/components/Quiz";
-import Verify1 from "@/components/Verify1";
+//import Verify1 from "@/components/Verify1";
+import Verify2 from "@/components/Verify2";
 
 export default function QuizStation() {
   return (
     <div>
-      <Verify1 />
+      <Verify2 />
     </div>
   );
 }
