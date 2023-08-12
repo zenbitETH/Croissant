80408e527a1a14f3562e29469ce13bb0622460e0
habacuc.eth <habacuc.vera@gmail.com>
2023-08-12 10:40:55 -0600

    quiz base component

--- packages/nextjs/components/Quiz.tsx
+++ packages/nextjs/components/Quiz.tsx
@@ -2,9 +2,22 @@ export default function Quiz() {
   return (
     <div className="overflow-hidden text-center h-screen grid items-center  relative">
       <div className="grid gap-5 mx-auto">
-        <div className="bg-bt/50 p-5 rounded-dd">
-          <div className="font-kan text-3xl text-l1 ">Tool used to interact with Daps and smart contracts:</div>
-          <div>Options</div>
+        <div className="bg-bt/50 p-10 rounded-dd">
+          <div className="font-kan text-4xl text-black ">Tool used to interact with Daps and smart contracts:</div>
+          <div className="grid grid-cols-3 pt-10 gap-5 ">
+            <span className="flex itemx-center">
+              <input type="radio" name="radio-1" className="radio" />
+              <div className="grid items-center pl-3 font-kum text-2xl text-black">Hammer</div>
+            </span>
+            <span className="flex itemx-center">
+              <input type="radio" name="radio-1" className="radio" checked />
+              <div className="grid items-center pl-3 font-kum text-2xl text-black">E-mail</div>
+            </span>
+            <span className="flex itemx-center">
+              <input type="radio" name="radio-1" className="radio" checked />
+              <div className="grid items-center pl-3 font-kum text-2xl text-black">Wallet</div>
+            </span>
+          </div>
         </div>
       </div>
     </div>
